d5abe450aa65280864e61ec62b8230125df9f36a
Sompob Chanintornpipat <sompob.chanintornpipat@gmail.com>
2019-07-10 20:43:37 +0700

    init project

---
+++ src/main/java/com/sompob/captcha2/Captcha2Application.java
@@ -0,0 +1,13 @@
+package com.sompob.captcha2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Captcha2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Captcha2Application.class, args);
+	}
+
+}
